9c82809431e22e79420ff8ebd375baafa05cc8df
puffsnow <brucechen2@gmail.com>
2022-06-18 12:31:37 +0800

    feat(users): users controller actions for list and change user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def become
+    user = User.find(params[:id])
+    user_session = UserSession.create(user: user)
+
+    session['auth_token'] = user_session.token
+
+    redirect_to users_path
+  end
+end
